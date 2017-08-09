75133bdfb630c73fe32349f65239cab5dddfc664
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-08-09 23:23:17 +0200

    roundtrip tests

--- twobody/celestial/tests/test_anomaly.py
+++ twobody/celestial/tests/test_anomaly.py
@@ -1,13 +1,11 @@
-# Standard library
-import os
-import sys
-
 # Third-party
 import numpy as np
+import pytest
 
 # Project
-from ..core import (mean_anomaly_from_eccentric_anomaly,
-                    eccentric_anomaly_from_mean_anomaly_Newton1)
+from .. import anomaly
+from ..anomaly import (mean_anomaly_from_eccentric_anomaly,
+                       eccentric_anomaly_from_mean_anomaly)
 
 N = 16
 
@@ -23,30 +21,50 @@ def test_mean_anomaly_from_eccentric_anomaly():
     # call with arrays
     M = mean_anomaly_from_eccentric_anomaly(Es, es)
 
-def test_eccentric_anomaly_from_mean_anomaly_Newton1():
+@pytest.mark.parametrize("method", ["Newton1"])
+def test_eccentric_anomaly_from_mean_anomaly(method):
     M_grid = np.linspace(0, 2*np.pi, N)
     e_grid = np.linspace(0, 1, N)
     Ms, es = [x.ravel() for x in np.meshgrid(M_grid, e_grid)]
 
     # call with float
     for M, e in zip(Ms, es):
-        E = mean_anomaly_from_eccentric_anomaly(M, e)
+        E = eccentric_anomaly_from_mean_anomaly(M, e, method=method)
 
     # call with arrays
-    E = mean_anomaly_from_eccentric_anomaly(Ms, es)
+    E = eccentric_anomaly_from_mean_anomaly(Ms, es)
 
-def test_anomaly_roundtrip():
+@pytest.mark.parametrize("method", ["Newton1"])
+def test_anomaly_roundtrip(method):
+    func = getattr(anomaly,
+                   "eccentric_anomaly_from_mean_anomaly_{0}".format(method))
+    kw = dict(tol=1E-10, maxiter=128)
+
     # M -> E -> M
     M_grid = np.linspace(0, 2*np.pi, N)
     e_grid = np.linspace(0, 1 - 1E-9, N) # MAGIC NUMBER: 1E-9
     Ms, es = [x.ravel() for x in np.meshgrid(M_grid, e_grid)]
 
     for M, e in zip(Ms, es):
-        E = eccentric_anomaly_from_mean_anomaly_Newton1(M, e, tol=1E-10)
+        E = func(M, e, **kw)
         M2 = mean_anomaly_from_eccentric_anomaly(E, e)
         assert np.allclose(M, M2, atol=1E-14)
 
-    E = eccentric_anomaly_from_mean_anomaly_Newton1(Ms, es, tol=1E-10)
+    E = func(Ms, es, **kw)
     M2 = mean_anomaly_from_eccentric_anomaly(E, es)
     assert np.allclose(Ms, M2, atol=1E-14)
+
+    # E -> M -> E
+    E_grid = np.linspace(0, 2*np.pi, N)
+    e_grid = np.linspace(0, 1 - 1E-9, N) # MAGIC NUMBER: 1E-9
+    Es, es = [x.ravel() for x in np.meshgrid(E_grid, e_grid)]
+
+    for E, e in zip(Es, es):
+        M = mean_anomaly_from_eccentric_anomaly(E, e)
+        E2 = func(M, e, **kw)
+        assert np.allclose(E, E2, atol=1E-14)
+
+    M = mean_anomaly_from_eccentric_anomaly(Es, es)
+    E2 = func(M, es, **kw)
+    assert np.allclose(Es, E2, atol=1E-14)
 
