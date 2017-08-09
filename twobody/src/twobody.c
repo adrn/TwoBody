c094f3a254d8a4549ca6688a0e7d134864e86578
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-08-09 10:04:51 +0200

    add C src code

---
+++ twobody/src/twobody.c
@@ -0,0 +1,57 @@
+#include <math.h>
+
+double mean_anomaly_from_eccentric_anomaly(double E, double e) {
+    /*
+    Parameters
+    ----------
+    E : double [radian]
+        Eccentric anomaly.
+    e : double
+        Eccentricity.
+
+    Returns
+    -------
+    M : double [radian]
+        Mean anomaly.
+    */
+    return E - e * sin(E);
+}
+
+double eccentric_anomaly_from_mean_anomaly_Newton1(double M, double e,
+                                                   double tol, int maxiter) {
+    /*
+    tol = 1E-13
+    maxiter = 128
+
+    Parameters
+    ----------
+    Ms : double [radian]
+        Mean anomaly.
+    e : double
+        Eccentricity.
+    tol : double
+        Numerical tolerance used in iteratively solving for eccentric anomaly.
+    maxiter : int
+        Maximum number of iterations when iteratively solving for eccentric
+        anomaly.
+
+    Returns
+    -------
+    E : double [radian]
+        Eccentric anomaly.
+
+    */
+
+    double dM, E;
+
+    // initialization
+    E = M + e * sin(M);
+
+    for (int i=0; i<maxiter; i++) {
+        dM = M - mean_anomaly_from_eccentric_anomaly(E, e);
+        E = E + dM / (1. - e * cos(E));
+
+        if (abs(dM) < tol)
+            break;
+    }
+}
---
+++ twobody/src/twobody.h
@@ -0,0 +1,4 @@
+extern double mean_anomaly_from_eccentric_anomaly(double E, double e);
+extern double eccentric_anomaly_from_mean_anomaly_Newton1(double M, double e,
+                                                          double tol,
+                                                          int maxiter);
