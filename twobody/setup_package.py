665351cbda99448ad48d3a9210d8d0065a9e0eb7
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-01-09 12:09:08 -0500

    change build paths

--- twobody/setup_package.py
+++ twobody/setup_package.py
@@ -11,13 +11,13 @@ def get_extensions():
     cfg = setup_helpers.DistutilsExtensionArgs()
     cfg['include_dirs'].append('numpy')
     cfg['include_dirs'].append(mac_incl_path)
-    cfg['include_dirs'].append('twobody/celestial/')
+    cfg['include_dirs'].append('twobody/')
     cfg['extra_compile_args'].append('--std=gnu99')
-    cfg['sources'].append('twobody/celestial/wrap.pyx')
-    cfg['sources'].append('twobody/celestial/src/twobody.c')
-    exts.append(Extension('twobody.celestial.wrap', **cfg))
+    cfg['sources'].append('twobody/wrap.pyx')
+    cfg['sources'].append('twobody/src/twobody.c')
+    exts.append(Extension('twobody.wrap', **cfg))
 
     return exts
 
 def get_package_data():
-    return {'twobody.celestial': ['src/*.h', 'src/*.c', '*.pyx', '*.pxd']}
+    return {'twobody': ['data/*', 'src/*.h', 'src/*.c', '*.pyx', '*.pxd']}
