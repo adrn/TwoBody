9c3ccc061f9445ac971122ba825a877f377be30a
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-01-12 17:20:46 -0500

    many improvements and fix class

--- twobody/orbit.py
+++ twobody/orbit.py
@@ -1,24 +1,24 @@
 # Third-party
 import astropy.coordinates as coord
 from astropy.coordinates.matrix_utilities import matrix_product
+from astropy.time import Time
 import astropy.units as u
 import numpy as np
 from numpy import pi
 
 # Project
 from . import elements as elem
-from .transforms import get_t0, a1_sini, mf
 from .anomaly import (eccentric_anomaly_from_mean_anomaly,
                       true_anomaly_from_eccentric_anomaly)
-from .utils import _parse_time, ArrayProcessor
+from .utils import ArrayProcessor
 from .barycenter import Barycenter
 from .wrap import cy_rv_from_elements
-
+from .reference_plane import ReferencePlaneFrame
 
 __all__ = ['KeplerOrbit']
 
 
-class KeplerOrbit(object):
+class KeplerOrbit:
 
     def __init__(self, elements=None, elements_type='kepler',
                  barycenter=None, **kwargs):
@@ -46,13 +46,15 @@ class KeplerOrbit(object):
                             "OrbitalElements subclass.")
 
         if barycenter is not None and not isinstance(barycenter, Barycenter):
-            raise TypeError("barycenter must be a twobody.Barycenter instance.")
+            raise TypeError("barycenter must be a twobody.Barycenter instance")
 
         self.elements = elements
         self.barycenter = barycenter
 
     def __getattr__(self, name):
 
+        # This gives access to the orbital element components directly from the
+        # Orbit instance
         if hasattr(self.elements, name):
             return getattr(self.elements, name)
 
@@ -62,7 +64,9 @@ class KeplerOrbit(object):
 
     def unscaled_radial_velocity(self, time,
                                  anomaly_tol=None, anomaly_maxiter=None):
-        """Compute the radial velocity of the body at the specified times.
+        """Compute the unscaled radial velocity of the body at the specified
+        times relative to the barycenter or reference point, i.e. in the
+        reference plane system not in a solar system barycentric frame.
 
         Parameters
         ----------
@@ -84,11 +88,11 @@ class KeplerOrbit(object):
             Relative radial velocity - does not include systemtic velocity!
         """
         if anomaly_tol is None:
-            # TODO: config item?
+            # TODO: make this a config item?
             anomaly_tol = 1E-10
 
         if anomaly_maxiter is None:
-            # TODO: config item?
+            # TODO: make this a config item?
             anomaly_maxiter = 128
 
         # TODO: do we always want to use MJD? precision issues...
@@ -102,21 +106,42 @@ class KeplerOrbit(object):
                                  anomaly_tol, anomaly_maxiter)
         return np.atleast_1d(proc.prepare_result(rv))
 
-    def radial_velocity(self, time):
-        """Compute the ... TODO
+    def radial_velocity(self, time, anomaly_tol=None, anomaly_maxiter=None):
+        """Compute the radial velocity of the body at the specified times
+        relative to the barycenter or reference point, i.e. in the reference
+        plane system not in a solar system barycentric frame.
+
+        This should always be close (in a machine precision sense) to
+        ``orbit.reference_plane(time).``
+
+        When the barycenter is assumed to be at rest with respect to the
+        observer, this should be equivalent to
+        ``orbit.icrs(time).radial_velocity``
 
         Parameters
         ----------
         time : array_like, `astropy.time.Time`
             Array of times as barycentric MJD values, or an Astropy
             `~astropy.time.Time` object containing the times to evaluate at.
+        anomaly_tol : numeric (optional)
+            Tolerance passed to
+            `~twobody.celestial.eccentric_anomaly_from_mean_anomaly` for solving
+            for the eccentric anomaly. See default value in that function.
+        anomaly_maxiter : numeric (optional)
+            Maximum number of iterations to use in
+            `~twobody.celestial.eccentric_anomaly_from_mean_anomaly` for solving
+            for the eccentric anomaly. See default value in that function.
         """
 
-        # TODO: barycenter motion
-        return self.K * self.unscaled_radial_velocity(time) # + self.barycenter.
+        if self.barycenter is not None:
+            rv0 = self.barycenter.origin.radial_velocity
+        else:
+            rv0 = 0 * u.km/u.s
+
+        return self.K * self.unscaled_radial_velocity(time) + rv0
 
     def reference_plane(self, time):
-        """Compute the orbit at specified times in the two body barycentric
+        """Compute the orbit at specified times in the two-body barycentric
         frame aligned with the reference plane coordinate system (XYZ).
 
         Parameters
@@ -128,17 +153,14 @@ class KeplerOrbit(object):
 
         # mean anomaly
         with u.set_enabled_equivalencies(u.dimensionless_angles()):
-            dt = time - self.t0
-            M = 2*pi * dt / self.P - self.M0
-            # TODO: should I do this:
-            # M = coord.Angle(M.to(u.radian)).wrap_at(360*u.deg)
+            M = 2*pi * (time.tcb - self.t0.tcb) / self.P - self.M0
             M = M.to(u.radian)
 
         # eccentric anomaly
-        E = eccentric_anomaly_from_mean_anomaly(M, self.e) * u.rad
+        E = eccentric_anomaly_from_mean_anomaly(M, self.e)
 
         # true anomaly
-        f = true_anomaly_from_eccentric_anomaly(E, self.e) * u.rad
+        f = true_anomaly_from_eccentric_anomaly(E, self.e)
 
         # distance from center of mass to orbiting body
         r = self.a * (1. - self.e * np.cos(E))
@@ -158,7 +180,7 @@ class KeplerOrbit(object):
 
         # Construct rotation matrix to take the orbit from the orbital plane
         # system (xyz) to the reference plane system (XYZ):
-        R1 = coord.matrix_utilities.rotation_matrix(self.omega, axis='z')
+        R1 = coord.matrix_utilities.rotation_matrix(-self.omega, axis='z')
         R2 = coord.matrix_utilities.rotation_matrix(self.i, axis='x')
         R3 = coord.matrix_utilities.rotation_matrix(self.Omega, axis='z')
         Rot = R3 @ R2 @ R1
@@ -166,145 +188,84 @@ class KeplerOrbit(object):
         # Rotate to the reference plane system
         XYZ = coord.CartesianRepresentation(matrix_product(Rot, xyz.xyz))
         VXYZ = coord.CartesianDifferential(matrix_product(Rot, vxyz.d_xyz))
+        XYZ = XYZ.with_differentials(VXYZ)
 
-        return XYZ.with_differentials(VXYZ)
+        kw = dict()
+        if self.barycenter is not None:
+            kw['origin'] = self.barycenter.origin
+        return ReferencePlaneFrame(XYZ, **kw)
 
     def icrs(self, time):
-        pass
+        """
+        """
+        rp = self.reference_plane(time)
 
-    def at_times(self, time):
+        icrs_cart = rp.transform_to(coord.ICRS).cartesian
+        icrs_pos = icrs_cart.without_differentials()
+        icrs_vel = icrs_cart.differentials['s']
 
+        bary_cart = self.barycenter.origin.cartesian
+        bary_vel = bary_cart.differentials['s']
 
-        # Account for motion of the barycenter
-        dt = times - self.barycenter.t0
+        dt = time - self.barycenter.t0
+        dx = (bary_vel * dt).to_cartesian()
 
-        # TODO: transform
-        frame = self.barycenter.reference_plane_frame
-        bc = self.barycenter.frame.transform_to(reference_plane_frame)
-        v_bary = bc.cartesian.differentials['s']
+        new_pos = icrs_pos + dx
+        new_vel = icrs_vel + bary_vel
 
-        XYZ = XYZ + bc.distance + v_bary * dt
-        VXYZ = coord.CartesianDifferential(VXYZ.d_xyz + v_bary.d_xyz)
+        return coord.ICRS(new_pos.with_differentials(new_vel))
 
+    def plot_rv(self, t, ax=None, rv_unit=None, t_kwargs=None,
+                plot_kwargs=None):
+        """Plot the RV curve at the specified times.
 
-    # def t0(self, ref_mjd):
-    #     """Un-mod the phase at pericenter, ``phi0``, to a time closest to the
-    #     specified reference epoch.
+        Parameters
+        ----------
+        t : array_like, `~astropy.time.Time`
+            Time array. Either in BMJD or as an Astropy time.
+        ax : `~matplotlib.axes.Axes`, optional
+            The axis to draw on (default is to grab the current
+            axes using `~matplotlib.pyplot.gca`).
+        rv_unit : `~astropy.units.UnitBase`, optional
+            Units to plot the radial velocities in (default is km/s).
+        t_kwargs : dict, optional
+            Keyword arguments passed to :class:`astropy.time.Time` with the
+            input time array. For example, ``dict(format='mjd', scale='tcb')``
+            for Barycentric MJD.
+        plot_kwargs : dict, optional
+            Any additional arguments or style settings passed to
+            :func:`matplotlib.pyplot.plot`.
 
-    #     Parameters
-    #     ----------
-    #     ref_mjd : numeric
-    #         Reference time in Barycentric MJD to get the pericenter time
-    #         ``t0`` relative to.
+        Returns
+        -------
+        ax : `~matplotlib.axes.Axes`
+            The matplotlib axes object that the RV curve was drawn on.
 
-    #     Returns
-    #     -------
-    #     t0 : `~astropy.time.Time`
-    #         Pericenter time closest to input epoch.
+        """
 
-    #     """
-    #     return get_t0(self.phi0, self.P, ref_mjd)
+        if ax is None:
+            import matplotlib.pyplot as plt
+            ax = plt.gca()
 
-    # def _generate_rv_curve(self, t):
-    #     if isinstance(t, at.Time):
-    #         _t = t.tcb.mjd
-    #     else:
-    #         _t = t
+        if rv_unit is None:
+            rv_unit = u.km/u.s
 
-    #     rv = rv_from_elements(times=_t,
-    #                           P=self.P.to(u.day).value,
-    #                           K=self.K.to(u.m/u.s).value,
-    #                           e=self.ecc,
-    #                           omega=self.omega.to(u.radian).value,
-    #                           phi0=self.phi0.to(u.radian).value,
-    #                           anomaly_tol=self.anomaly_tol)
+        if t_kwargs is None:
+            t_kwargs = dict(format='mjd', scale='tcb')
 
-    #     if self.trend is not None:
-    #         v_trend = self.trend(_t).to(u.m/u.s).value
-    #         rv += v_trend
+        if plot_kwargs is None:
+            plot_kwargs = dict()
 
-    #     return rv
+        style = plot_kwargs.copy()
+        style.setdefault('linestyle', '-')
+        style.setdefault('alpha', 0.5)
+        style.setdefault('marker', None)
 
-    # def generate_rv_curve(self, t):
-    #     """Generate a radial velocity curve evaluated at the specified times
-    #     with the instantiated parameters.
+        if not isinstance(t, Time):
+            t = Time(t, **t_kwargs)
+        rv = self.generate_rv_curve(t).to(rv_unit).value
 
-    #     Parameters
-    #     ----------
-    #     t : array_like, `~astropy.time.Time`
-    #         Time array. Either in BMJD or as an Astropy time.
+        _t = getattr(getattr(t, t_kwargs['scale']), t_kwargs['format'])
+        ax.plot(_t, rv, **style)
 
-    #     Returns
-    #     -------
-    #     rv : astropy.units.Quantity [speed]
-    #     """
-    #     rv = self._generate_rv_curve(t)
-    #     return (rv*u.m/u.s).to(u.km/u.s)
-
-    # def __call__(self, t):
-    #     return self.generate_rv_curve(t)
-
-    # def plot(self, t, ax=None, rv_unit=None, t_kwargs=None, plot_kwargs=None):
-    #     """Plot the RV curve at the specified times.
-
-    #     Parameters
-    #     ----------
-    #     t : array_like, `~astropy.time.Time`
-    #         Time array. Either in BMJD or as an Astropy time.
-    #     ax : `~matplotlib.axes.Axes`, optional
-    #         The axis to draw on (default is to grab the current
-    #         axes using `~matplotlib.pyplot.gca`).
-    #     rv_unit : `~astropy.units.UnitBase`, optional
-    #         Units to plot the radial velocities in (default is km/s).
-    #     t_kwargs : dict, optional
-    #         Keyword arguments passed to :class:`astropy.time.Time` with the
-    #         input time array. For example, ``dict(format='mjd', scale='tcb')``
-    #         for Barycentric MJD.
-    #     plot_kwargs : dict, optional
-    #         Any additional arguments or style settings passed to
-    #         :func:`matplotlib.pyplot.plot`.
-
-    #     Returns
-    #     -------
-    #     ax : `~matplotlib.axes.Axes`
-    #         The matplotlib axes object that the RV curve was drawn on.
-
-    #     """
-
-    #     if ax is None:
-    #         import matplotlib.pyplot as plt
-    #         ax = plt.gca()
-
-    #     if rv_unit is None:
-    #         rv_unit = u.km/u.s
-
-    #     if t_kwargs is None:
-    #         t_kwargs = dict(format='mjd', scale='tcb')
-
-    #     if plot_kwargs is None:
-    #         plot_kwargs = dict()
-
-    #     style = plot_kwargs.copy()
-    #     style.setdefault('linestyle', '-')
-    #     style.setdefault('alpha', 0.5)
-    #     style.setdefault('marker', None)
-
-    #     if not isinstance(t, at.Time):
-    #         t = at.Time(t, **t_kwargs)
-    #     rv = self.generate_rv_curve(t).to(rv_unit).value
-
-    #     _t = getattr(getattr(t, t_kwargs['scale']), t_kwargs['format'])
-    #     ax.plot(_t, rv, **style)
-
-    #     return ax
-
-    # # --------------------------------------------------------------------------
-    # # Computed attributes
-
-    # @property
-    # def a1_sini(self):
-    #     return a1_sini(self.P, self.K, self.ecc)
-
-    # @property
-    # def mf(self):
-    #     return mf(self.P, self.K, self.ecc)
+        return ax
--- twobody/tests/test_orbit.py
+++ twobody/tests/test_orbit.py
@@ -1,5 +1,6 @@
 # Third-party
 from astropy.time import Time
+import astropy.coordinates as coord
 import astropy.units as u
 from astropy.tests.helper import quantity_allclose
 import numpy as np
@@ -8,53 +9,90 @@ import pytest
 try:
     import matplotlib.pyplot as plt
     HAS_MPL = True
-except:
+except ImportError:
     HAS_MPL = False
 
 # Package
-from ..orbit import RVOrbit
-from ..trends import VelocityTrend2 # linear
+from ..orbit import KeplerOrbit
+from ..barycenter import Barycenter
 
-# default orbit parameters
-default_pars = dict(P=30.*u.day, K=100.*u.m/u.s, ecc=0.11239,
-                    omega=0.*u.radian, phi0=0.25524*u.radian)
 
-def test_init_rvorbit():
+def get_random_orbit(rnd=None, barycenter=None):
+    if rnd is None:
+        rnd = np.random.RandomState()
 
-    # without trend
-    orbit = RVOrbit(**default_pars)
+    P = 10**rnd.uniform(0, 3)*u.day
+    K = np.abs(rnd.normal(0, 100))*u.km/u.s
+    e = rnd.uniform(0, 1)
+    i = rnd.uniform(0, 180)*u.deg
+    n = 2*np.pi/P
+    a = K * np.sqrt(1-e**2) / (n * np.sin(i))
+    return KeplerOrbit(P=P, a=a, e=e, i=i,
+                       omega=rnd.uniform(0, 360)*u.deg,
+                       Omega=rnd.uniform(0, 360)*u.deg,
+                       M0=rnd.uniform(0, 360)*u.deg,
+                       barycenter=barycenter)
 
-    t = np.random.uniform(55612., 55792, 1024)
-    t.sort()
-    rv = orbit.generate_rv_curve(t)
-    rv2 = orbit(t)
-    assert quantity_allclose(rv, rv2)
 
-    # with trend - check t0
-    pars = dict(P=1.*u.day, K=100.*u.m/u.s, ecc=0.,
-                omega=0.*u.radian, phi0=90*u.degree)
-    trend = VelocityTrend2(100.*u.km/u.s, 1*u.km/u.s/u.day, t0=55831.)
-    orbit1 = RVOrbit(trend=trend, **pars)
-    assert quantity_allclose(orbit1(55831.), 100 * u.km/u.s)
+def test_init():
+    pass
 
-    # get pericenter time - check something?!
-    orbit = RVOrbit(**pars)
-    mjd_grid = [57975.15, 57975.95, 57978.55]
-    expected = [57975.25, 57976.25, 57978.25]
-    mjd0 = [orbit.t0(x).tcb.mjd for x in mjd_grid]
-    assert np.allclose(mjd0, expected)
 
-def test_attributes():
-    orbit = RVOrbit(**default_pars)
+def test_radial_velocity():
+    # Compare the short-circuit RV to the full calculation
 
-    for attr in ['a1_sini', 'mf']:
-        getattr(orbit, attr)
+    rnd = np.random.RandomState(seed=42)
+    n_iter = 128
+
+    bc_kw = dict(ra=210.413231*u.deg,
+                 dec=-61.3412*u.deg,
+                 distance=101*u.pc)
+
+    # at rest w.r.t. sun
+    bc1 = Barycenter(origin=coord.ICRS(**bc_kw,
+                                       pm_ra_cosdec=0*u.mas/u.yr,
+                                       pm_dec=0.*u.mas/u.yr,
+                                       radial_velocity=0*u.km/u.s),
+                     t0=Time('J2018.0'))
+
+    bc2 = Barycenter(origin=coord.ICRS(**bc_kw,
+                                       pm_ra_cosdec=1000*u.mas/u.yr,
+                                       pm_dec=1000.*u.mas/u.yr,
+                                       radial_velocity=-260*u.km/u.s),
+                     t0=Time('J2018.0'))
+
+    times = Time('J2018.0') + np.linspace(-100, 100, 128) * u.day
+
+    # Even when barycenter is moving, rv1 and rv2 should be the same
+    for n in range(n_iter):
+        orb = get_random_orbit(rnd, bc2)
+
+        rv1 = orb.radial_velocity(times)
+        _rp = orb.reference_plane(times)
+        rv2 = -_rp.cartesian.differentials['s'].d_z + bc2.origin.radial_velocity
+
+        assert quantity_allclose(rv1, rv2, rtol=0, atol=1E-4*u.m/u.s)
+
+    # All RV's should be equivalent
+    for n in range(n_iter):
+        orb = get_random_orbit(rnd, bc1)
+
+        rv1 = orb.radial_velocity(times)
+        _rp = orb.reference_plane(times)
+        rv2 = -_rp.cartesian.differentials['s'].d_z
+        rv3 = orb.icrs(times).radial_velocity
+
+        assert quantity_allclose(rv1, rv2, rtol=0, atol=1E-4*u.m/u.s)
+        # We expect true differences because of projection effects
+        assert quantity_allclose(rv1, rv3, rtol=0, atol=1*u.km/u.s)
+
 
 @pytest.mark.skipif(not HAS_MPL, reason="matplotlib not installed")
 def test_plotting():
-    orbit = RVOrbit(**default_pars)
+    orbit = KeplerOrbit(P=100*u.day, a=1.*u.au, e=0.1,
+                        omega=0*u.deg, i=41*u.deg, Omega=0*u.deg)
 
-    mjd = np.linspace(56823.123, 57293.2345, 1024) # random MJD's
+    mjd = np.linspace(56823.123, 57293.2345, 1024)  # random MJD's
     t = Time(mjd, format='mjd', scale='utc')
 
     # plotting
