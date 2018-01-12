af4238d9446e46100e4e7a9673b8246ec0d8f57b
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-01-12 17:20:30 -0500

    shell of a class

--- twobody/barycenter.py
+++ twobody/barycenter.py
@@ -3,18 +3,29 @@ from astropy.constants import G
 from astropy.time import Time
 import astropy.units as u
 import numpy as np
-from numpy import pi
 
 # Project
 from .utils import _parse_time
-
 
 __all__ = ['Barycenter']
 
 
 class Barycenter:
 
-    def __init__(self, coords, t0):
-        self.coords = coords
-        self.t0 = _parse_time(t0)
+    def __init__(self, origin, t0=None):
+        """The location of the barycenter at the specified epoch.
 
+        Parameters
+        ----------
+        origin : `~astropy.coordinates.SkyCoord` or frame instance
+            TODO
+        t0 : numeric, `~astropy.coordinates.Time` (optional)
+            Reference epoch for the location of the barycenter. If a number is
+            passed in, it is assumed to be a solar system barycentric modified
+            julian date (BMJD). The default is J2000 if not specified.
+        """
+        self.origin = origin
+
+        if t0 is None:
+            t0 = Time('J2000')
+        self.t0 = _parse_time(t0)
