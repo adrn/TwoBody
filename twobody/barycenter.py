4da38186c75b27b98a3abfa3b7613787c4b6bdfd
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-01-16 15:08:27 -0500

    allow passing just radial velocity

--- twobody/barycenter.py
+++ twobody/barycenter.py
@@ -14,7 +14,7 @@ __all__ = ['Barycenter']
 
 class Barycenter:
 
-    def __init__(self, origin, t0=None):
+    def __init__(self, origin=None, t0=None, radial_velocity=None):
         """The location of the barycenter at the specified epoch.
 
         This class is used to specify a reference location and velocity of the
@@ -28,16 +28,22 @@ class Barycenter:
         ----------
         origin : `~astropy.coordinates.SkyCoord` or frame instance
             The sky position of and distance to the barycenter at the specified
-            epoch, ``t0``.
+            epoch, ``t0``. If only a barycentric radial velocity is known or
+            needed, pass in ``radial_velocity``.
         t0 : numeric, `~astropy.coordinates.Time` (optional)
             Reference epoch for the location of the barycenter. If a number is
             passed in, it is assumed to be a solar system barycentric modified
             julian date (BMJD). The default is J2000 if not specified.
+        radial_velocity : quantity_like (optional)
+            If the coordinates or distance to the barycenter are not known or
+            needed, you can just pass the radial velocity of the barycenter
+            instead.
 
         Examples
         --------
         >>> import astropy.units as u
         >>> from astropy.coordinates import SkyCoord
+        >>> b = Barycenter(radial_velocity=100*u.km/u.s)
         >>> b = Barycenter(origin=SkyCoord(ra=150*u.deg, dec=37*u.deg,
         ...                                distance=100*u.pc)) # assumes J2000
         >>> from astropy.time import Time
@@ -46,6 +52,16 @@ class Barycenter:
         ...                t0=Time(58912.481293, format='mjd'))
 
         """
+
+        if origin is not None and radial_velocity is not None:
+            raise ValueError("Pass one of: origin or radial_velocity, not both")
+
+        if origin is None and radial_velocity is None:
+            raise ValueError("You must pass one of origin or radial_velocity")
+
+        if origin is None:
+            origin = coord.ICRS(ra=0*u.deg, dec=0*u.deg, distance=np.nan*u.pc,
+                                radial_velocity=radial_velocity)
 
         if (not isinstance(origin, coord.SkyCoord) and
                 not isinstance(origin, coord.BaseCoordinateFrame)):
--- twobody/tests/test_barycenter.py
+++ twobody/tests/test_barycenter.py
@@ -25,6 +25,13 @@ def test_init():
                                          distance=100 * u.pc),
                    t0=Time(59123.1213, format='mjd'))
 
+    c = coord.SkyCoord(ra=150 * u.deg, dec=-11 * u.deg, distance=100 * u.pc)
+    with pytest.raises(ValueError):
+        Barycenter(origin=c, radial_velocity=100*u.km/u.s)
+
+    with pytest.raises(ValueError):
+        Barycenter()
+
     with pytest.raises(TypeError):
         Barycenter(origin=Time('J2000'))
 
