d0d24a808c57545a26b0f3782ccba8a0df4eae54
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-04-18 14:04:50 -0400

    use numpy get_include

--- twobody/setup_package.py
+++ twobody/setup_package.py
@@ -3,13 +3,15 @@ from distutils.core import Extension
 
 
 def get_extensions():
+    import numpy as np
+
     exts = []
 
     # malloc
     mac_incl_path = "/usr/include/malloc"
 
     cfg = defaultdict(list)
-    cfg['include_dirs'].append('numpy')
+    cfg['include_dirs'].append(np.get_include())
     cfg['include_dirs'].append(mac_incl_path)
     cfg['include_dirs'].append('twobody/')
     cfg['extra_compile_args'].append('--std=gnu99')
