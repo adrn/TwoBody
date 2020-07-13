abcd9e081d58153923cad9511434698da46f8559
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-07-13 17:07:35 -0400

    fix astropy utils import

--- twobody/utils.py
+++ twobody/utils.py
@@ -1,6 +1,6 @@
 # Third-party
 from astropy.time import Time
-from astropy.utils.shapes import check_broadcast
+from astropy.utils import check_broadcast
 import numpy as np
 
 __all__ = ['ArrayProcessor']
