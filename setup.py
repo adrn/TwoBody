383c6c6153472f5e3200608a4065a123703e78da
Adrian Price-Whelan <adrian.prw@gmail.com>
2024-08-18 19:29:03 -0400

    more packaging stuff

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ cfg = defaultdict(list)
 cfg["include_dirs"].append(np.get_include())
 cfg["include_dirs"].append("twobody/")
 cfg["extra_compile_args"].append("--std=gnu99")
-cfg["sources"].append("twobody/src/wrap.pyx")
+cfg["sources"].append("twobody/wrap.pyx")
 cfg["sources"].append("twobody/src/twobody.c")
 exts.append(Extension("twobody.wrap", **cfg))
 
--- twobody/__init__.py
+++ twobody/__init__.py
@@ -1,35 +1,14 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
 """
 A package for computing orbits and astronomical observables for binary stars,
 exoplanets, and other gravitational two-body systems.
 """
 
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
-# Enforce Python version check during package import.
-# This is the same check as the one at the top of setup.py
-import sys
-
-__minimum_python_version__ = "3.6"
-
-class UnsupportedPythonError(Exception):
-    pass
-
-if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
-    raise UnsupportedPythonError("packagename does not support Python < {}"
-                                 .format(__minimum_python_version__))
-
-if not _ASTROPY_SETUP_:
-    from .anomaly import *
-    from .barycenter import *
-    from .bary_trends import *
-    from .elements import *
-    from .orbit import *
-    from .reference_plane import *
-    from .transforms import *
-    from .units import *
+from ._version import *
+from .anomaly import *
+from .bary_trends import *
+from .barycenter import *
+from .elements import *
+from .orbit import *
+from .reference_plane import *
+from .transforms import *
+from .units import *
